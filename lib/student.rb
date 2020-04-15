dece8511548b940ae02fa91a797a84280d6fa657
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-15 02:45:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,12 +40,17 @@ attr_reader :id
   end
 
   def self.create(:name, :grade)
-    student = student.new(name, grade)
-    student.save
-    student
+    new_student = self.new(name, grade)
+    new_student.save
+    new_student
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.find_by_name(name)
