1ae9dc68a965c329eff363887f91c410dcb27a05
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-15 02:30:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,19 @@
 require_relative "../config/environment.rb"
 
 class Student
+attr_accessor :name, :grade
+attr_reader :idea
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+
 
 
 end
+
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]
